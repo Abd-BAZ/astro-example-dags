d2cd2ab26dfedad1d64aa3fd25f5bdd102888a77
bigbazli3112@gmail.com <bigbazli3112@gmail.com>
2025-11-03 06:25:40 +0000

    Stop RUnning

--- dags/example_astronauts.py
+++ dags/example_astronauts.py
@@ -36,7 +36,7 @@ import pandas as pd
 # Define the basic parameters of the DAG, like schedule and start_date
 @dag(
     start_date=datetime(2024, 1, 1),
-    schedule="@daily",
+    schedule=None,  # Set to None to disable automatic runs
     catchup=False,
     doc_md=__doc__,
     default_args={"owner": "Astro", "retries": 3},
